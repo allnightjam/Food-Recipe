5864c4c8f6c99c83f740f42e0e00f3213b58fafa
Joyce Ann Miguel <joyce.a.miguel@gmail.com>
2023-09-13 10:55:34 -0400

    adding modal code and the database

--- public/css/scripts/modal.js
+++ public/css/scripts/modal.js
@@ -2,13 +2,31 @@ document.addEventListener("DOMContentLoaded", function () {
   const modal = document.getElementById("meal-modal");
   const mealContainers = document.querySelectorAll(".meals-container");
   const closeButton = document.querySelector(".close");
+  const modalContent = document.querySelector(".modal-content");
 
   mealContainers.forEach(function (container) {
-    container.addEventListener("click", function () {
-      modal.style.display= "block";
-      <div class="meal-body">
-        <%= meal.body %>
-      </div>
+    container.addEventListener("click", async function () {
+      try {
+        const response = await fetch('/api/meals');
+        if (response.status<200 || response.status >= 300) {
+          throw new Error("Modal meals error");
+        }
+
+        const mealsData = await response.json();
+
+        const meal = mealsData[index];
+
+        modalContent.innerHTML = `
+          <span class="close">&times;</span>
+          <h2>${meal.name}</h2
+          <img src="${meal.photo_url}">
+          <p>${meal.body}</p>
+        `;
+        
+    modal.style.display= "block";
+    } catch (error) {
+      console.error("Error fetching meal data:", error);
+    }
     });
   });
 
@@ -20,5 +38,5 @@ document.addEventListener("DOMContentLoaded", function () {
     if (event.target === modal) {
       modal.style.display = "none";
     }
-  })
-})
+  });
+});
