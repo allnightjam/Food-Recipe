a6146eaa0645521091b2690a5bb5dedc4b515205
Joyce Ann Miguel <joyce.a.miguel@gmail.com>
2023-09-14 11:43:05 -0400

    added search function

---
+++ public/css/scripts/search.js
@@ -0,0 +1,24 @@
+const searchInput = document.getElementById('search-input');
+const mealContainers = document.querySelectorAll(".meals-container");
+
+searchInput.addEventListener("input", function(event) {
+  const searchTerm = event.target.value.toLowerCase();
+  mealContainers.forEach(function(container) {
+    const mealNames = container.querySelectorAll(".meal-name h3");
+    let showContainer = false;
+    
+  mealNames.forEach(function(mealName) {
+    const mealNameText = mealName.innerText.toLowerCase();
+    console.log("meal:", mealNameText, "text:", searchTerm);
+    if (mealNameText.includes(searchTerm)) {
+      showContainer = true;  
+    }
+    });
+
+    if (showContainer) {
+      container.style.display = "block";
+    } else {
+      container.style.display = "none";
+    }
+  });
+});
